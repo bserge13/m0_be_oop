004f16705507d8fd6d3bf4688f99447a745cff79
Blake Sergesketter <blakesergesketter@gmail.com>
2023-04-02 17:07:59 -0500

    Initial commit

--- oop.rb
+++ oop.rb
@@ -3,6 +3,14 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn 
+attr_reader :name 
+def initialize(name)
+    @name = name 
+    @color = "Silver"
+end 
+
+def say
 
 
 #  Write a class called Vampire
@@ -11,6 +19,18 @@
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+class Vampire 
+    attr_reader :name 
+    def initialize(name, pet)
+        @name = name
+        @pet = "Bat"
+        @thirsty = true 
+    end 
+
+    def new_pet(new_pet)
+        @pet = new_pet 
+    end 
+end 
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -18,6 +38,18 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+
+class Dragon 
+    attr_reader :name 
+    def initialize(name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color 
+        @hungry = true 
+    end
+    def eat_something 
+        @hungry = false 
+        if 
 
 
 #  Write a Hobbit class
@@ -28,3 +60,18 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit 
+    attr_reader :name 
+    def initialize(name, disposition)
+        @name = name
+        @disposition = disposition
+        @age = 0 
+        @is_adult = false 
+        @is_old = false 
+        @has_ring = false 
+    end 
+
+    def celebrate_birthday
+        @age += 1 
+    end 
